d2a8761694a02ec00dd266e303f6a1de03fb66cf
zlouis <jiannchyao@gmail.com>
2017-10-24 15:10:26 -0400

    test

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ var con = mysql.createConnection({
 
   host: "t89yihg12rw77y6f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  Username: "swvr0i1j9ny720mk",
+  user: "swvr0i1j9ny720mk",
   password: "d6sguicbhg2x3x48",
 });
 
