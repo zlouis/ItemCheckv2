d2473b34c13e8af56809387217ddb39bfd56c82d
zlouis <jiannchyao@gmail.com>
2017-10-24 15:08:15 -0400

    jaws db id

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ var con = mysql.createConnection({
 
   host: "t89yihg12rw77y6f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "swvr0i1j9ny720mk",
+  Username: "swvr0i1j9ny720mk",
   password: "d6sguicbhg2x3x48",
 });
 
