c0c5f88b008b05ee79e683c9293f59ebab9b0969
zlouis <jiannchyao@gmail.com>
2017-10-24 22:26:26 -0400

    test

--- server.js
+++ server.js
@@ -189,6 +189,7 @@ app.delete('/delete', function(req,res){
 });
 
 // Open Server
+var PORT = process.env.PORT || 3000;
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
