8ff19ef601b83274fc3e51f2b1d2859815a5df84
zlouis <jiannchyao@gmail.com>
2017-10-30 19:53:19 -0400

    testing

--- server.js
+++ server.js
@@ -79,115 +79,115 @@ app.get('/', function (req, res) {
 
 
 
-//post to database with link
-app.post('/submit', function (req, res) {
-   var createLink = {
-    link: req.body.link,
-   }
-   var createItem = {
+// //post to database with link
+// app.post('/submit', function (req, res) {
+//    var createLink = {
+//     link: req.body.link,
+//    }
+//    var createItem = {
 
-   }
-    con.query('INSERT INTO links SET ?', createLink,
-        function (err, result) {
-            if (err) throw err;
-            console.log(req.body)
-            res.send('Link added to database with ID: ' + result.insertId);
-        }
-    );
-});
+//    }
+//     con.query('INSERT INTO links SET ?', createLink,
+//         function (err, result) {
+//             if (err) throw err;
+//             console.log(req.body)
+//             res.send('Link added to database with ID: ' + result.insertId);
+//         }
+//     );
+// });
 
-//GET ROUTE for scraping second link
-// router.get('/scrape');
+// //GET ROUTE for scraping second link
+// // router.get('/scrape');
 
-//scrape second link
-app.post('/scrape', function(req, res){
+// //scrape second link
+// app.post('/scrape', function(req, res){
 
-  url = req.body.link;
-  var createUrl = {
-    link: req.body.link
-  }
-  var createItem= {
-    item: req.body.item
-  }
-  var createJson= {
-    reviews:"",
-  }
+//   url = req.body.link;
+//   var createUrl = {
+//     link: req.body.link
+//   }
+//   var createItem= {
+//     item: req.body.item
+//   }
+//   var createJson= {
+//     reviews:"",
+//   }
 
-//starts to scrape the user input link
-   request(url, function(err, response, html) {
+// //starts to scrape the user input link
+//    request(url, function(err, response, html) {
 
-      if(!err){
-       var $= cheerio.load(html);
+//       if(!err){
+//        var $= cheerio.load(html);
 
-      var reviews;
-      var json ={
-        reviews:"",
-      };
-      //captures target
-      $('#reviews-text').filter(function(){
-        //Holds data
-        var data=$(this)
-
-
+//       var reviews;
+//       var json ={
+//         reviews:"",
+//       };
+//       //captures target
+//       $('#reviews-text').filter(function(){
+//         //Holds data
+//         var data=$(this)
 
 
-        reviews = data.text();
-        createJson=reviews;
-        json.reviews=reviews;
-
-        if(json.reviews === "Reviews") {
-          console.log("reviewed checked and item has review")
-        } else {
-          console.log("No reviews availible")
 
 
-        }
+//         reviews = data.text();
+//         createJson=reviews;
+//         json.reviews=reviews;
+
+//         if(json.reviews === "Reviews") {
+//           console.log("reviewed checked and item has review")
+//         } else {
+//           console.log("No reviews availible")
+
+
+//         }
 
      
 
-      })
-        var sql= "INSERT INTO storage (link, item, stock) VALUES ('" +createUrl.link +"','"+createItem.item+"','"+json.reviews+"')"
-        con.query(sql,
-        function (err, result) {
-            if (err) throw err;
-            console.log(req.body)
-            res.send('Link added to database with ID: ' + result.insertId + " " + "go to /index to see database");
-        }
-    );
-    }
+//       })
+//         var sql= "INSERT INTO storage (link, item, stock) VALUES ('" +createUrl.link +"','"+createItem.item+"','"+json.reviews+"')"
+//         con.query(sql,
+//         function (err, result) {
+//             if (err) throw err;
+//             console.log(req.body)
+//             res.send('Link added to database with ID: ' + result.insertId + " " + "go to /index to see database");
+//         }
+//     );
+//     }
 
 
 
-  })
-// res.send('Check your console!')
+//   })
+// // res.send('Check your console!')
 
-// res.sendfile('views/forms.html')
-})
+// // res.sendfile('views/forms.html')
+// })
 
 
-//prints database to page
-app.get('/index', function(req,res) {
+// //prints database to page
+// app.get('/index', function(req,res) {
 
-    con.query('SELECT * FROM storage;', function(err, data) {
-      if (err) throw err;
+//     con.query('SELECT * FROM storage;', function(err, data) {
+//       if (err) throw err;
 
-      //test it
-      //console.log('The solution is: ', data);
+//       //test it
+//       //console.log('The solution is: ', data);
 
-      //test it
-      //res.send(data);
+//       //test it
+//       //res.send(data);
 
-      res.render('index', {storage : data});
-    });
-});
+//       res.render('index', {storage : data});
+//     });
+// });
 
-//delete data entry
-app.delete('/delete', function(req,res){
-    con.query('DELETE FROM storage WHERE id = ?', [req.body.id], function(err, result) {
-      if (err) throw err;
-      res.redirect('/index');
-    });
-});
+// //delete data entry
+// app.delete('/delete', function(req,res){
+//     con.query('DELETE FROM storage WHERE id = ?', [req.body.id], function(err, result) {
+//       if (err) throw err;
+//       res.redirect('/index');
+//     });
+// });
 
 // Open Server
 var PORT = process.env.PORT || 3000;
