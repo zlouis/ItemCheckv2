8075af4cf5fafa5b95d6b59da4c4c299a5165d14
zlouis <jiannchyao@gmail.com>
2017-10-30 19:50:36 -0400

    new server

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE mywarehouse;
+USE mywarehouse;
+
+CREATE TABLE storage
+(
+  id int NOT NULL AUTO_INCREMENT,
+PRIMARY KEY(id),
+link VARCHAR(255),
+item VARCHAR(255),
+stock VARCHAR(255)
+);
