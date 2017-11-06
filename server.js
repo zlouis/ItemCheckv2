45971490ccfcc8d3785ff99c085c5b09de975f7f
zlouis <jiannchyao@gmail.com>
2017-11-06 13:34:46 -0500

    test

--- server.js
+++ server.js
@@ -88,18 +88,18 @@ if(process.env.JAWSDB_URL) {
     //         });
     // });
 
-      connection.query('USE l80k3j1waol9ialw', function (err) {
-          if (err) throw err;
-          connection.query('CREATE TABLE IF NOT EXISTS storage('
-              + 'id INT NOT NULL AUTO_INCREMENT,'
-              + 'PRIMARY KEY(id),'
-              + 'link VARCHAR(255),'
-              + 'item VARCHAR(255),'
-              + 'stock VARCHAR(255)'
-              +  ')', function (err) {
-                  if (err) throw err;
-              });
-      });
+      // connection.query('USE l80k3j1waol9ialw', function (err) {
+      //     if (err) throw err;
+      //     connection.query('CREATE TABLE IF NOT EXISTS storage('
+      //         + 'id INT NOT NULL AUTO_INCREMENT,'
+      //         + 'PRIMARY KEY(id),'
+      //         + 'link VARCHAR(255),'
+      //         + 'item VARCHAR(255),'
+      //         + 'stock VARCHAR(255)'
+      //         +  ')', function (err) {
+      //             if (err) throw err;
+      //         });
+      // });
 
 app.get('/', function (req, res) {
   res.render('index');
