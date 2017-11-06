76dee6d34271ffaea0b831d61317648b9fd228bf
zlouis <jiannchyao@gmail.com>
2017-11-06 13:27:21 -0500

    test

--- server.js
+++ server.js
@@ -55,14 +55,14 @@ if(process.env.JAWSDB_URL) {
 //   password: "",
 // });
 // Set up Mysql
-// var connect = mysql.createConnection({
+var connect = mysql.createConnection({
 
-//   host: "ysp9sse09kl0tzxj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//   port: 3306,
-//   user: " dppwdlfvwb73o8gb",
-//   password: "zhb7s1mfjp0xy4jx",
-//   database: "l80k3j1waol9ialw"
-// });
+  host: "ysp9sse09kl0tzxj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: " dppwdlfvwb73o8gb",
+  password: "zhb7s1mfjp0xy4jx",
+  database: "l80k3j1waol9ialw"
+});
 
 
 
@@ -178,47 +178,37 @@ app.post('/scrape', function(req, res){
         
 
       })
-    //     $('.server-error').filter(function() {
-    //           //Holds data
-    //     var data=$(this)
+        $('.server-error').filter(function() {
+              //Holds data
+        var data=$(this)
 
-    //     reviews = data.text();
-    //     createJson.reviews=reviews;
-    //     captureReview=reviews;
+        reviews = data.text();
+        createJson.reviews=reviews;
+        captureReview=reviews;
 
-    //     if(captureReview != "Reviews") {
-    //     console.log("Has"+ "" +captureReview)
-    //     var sql= "INSERT INTO storage (link, item, stock) VALUES ('" +createUrl.link +"','"+createItem.item+"','"+captureReview+"')"
-    //     connection.query(sql,
-    //     function (err, result) {
-    //         if (err) throw err;
-    //         console.log(req.body)
-    //         res.send('Link added to database with ID: ' + result.insertId + " " + "go to /index to see database");
-    //     }
-    // );
-    //     } else {
-    //       console.log("no"+ "" +captureReview)
-    //     }
+        if(captureReview != "Reviews") {
+        console.log("Has"+ "" +captureReview)
+        var sql= "INSERT INTO storage (link, item, stock) VALUES ('" +createUrl.link +"','"+createItem.item+"','"+captureReview+"')"
+        connection.query(sql,
+        function (err, result) {
+            if (err) throw err;
+            console.log(req.body)
+            res.send('Link added to database with ID: ' + result.insertId + " " + "go to /index to see database");
+        }
+    );
+        } else {
+          console.log("no"+ "" +captureReview)
+        }
 
         
 
      
 
-    //   })
+      })
     } 
 
   })
-    //        var sql= "INSERT INTO storage (link, item, stock) VALUES ('" +createUrl.link +"','"+createItem.item+"','"+createJson.reviews+"')"
-    //     connection.query(sql,
-    //     function (err, result) {
-    //         if (err) throw err;
-    //         console.log(req.body)
-    //         res.send('Link added to database with ID: ' + result.insertId + " " + "go to /index to see database");
-    //     }
-    // );
-// res.send('Check your console!')
 
-// res.sendfile('views/forms.html')
 })
 
 
