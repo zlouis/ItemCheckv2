85eb3df377b8fd4c141dd426ad4296ddd1ac7230
zlouis <jiannchyao@gmail.com>
2017-11-06 12:39:14 -0500

    now working with refresher to check item

--- server.js
+++ server.js
@@ -75,31 +75,31 @@ if(process.env.JAWSDB_URL) {
 //     console.log('connected as id ' + connection.threadId);
 // });
     //local database testing
-    // connection.query('USE warehouse', function (err) {
-    //     if (err) throw err;
-    //     connection.query('CREATE TABLE IF NOT EXISTS storage('
-    //         + 'id INT NOT NULL AUTO_INCREMENT,'
-    //         + 'PRIMARY KEY(id),'
-    //         + 'link VARCHAR(255),'
-    //         + 'item VARCHAR(255),'
-    //         + 'stock VARCHAR(255)'
-    //         +  ')', function (err) {
-    //             if (err) throw err;
-    //         });
-    // });
+    connection.query('USE warehouse', function (err) {
+        if (err) throw err;
+        connection.query('CREATE TABLE IF NOT EXISTS storage('
+            + 'id INT NOT NULL AUTO_INCREMENT,'
+            + 'PRIMARY KEY(id),'
+            + 'link VARCHAR(255),'
+            + 'item VARCHAR(255),'
+            + 'stock VARCHAR(255)'
+            +  ')', function (err) {
+                if (err) throw err;
+            });
+    });
 
-      connection.query('USE l80k3j1waol9ialw', function (err) {
-          if (err) throw err;
-          connection.query('CREATE TABLE IF NOT EXISTS storage('
-              + 'id INT NOT NULL AUTO_INCREMENT,'
-              + 'PRIMARY KEY(id),'
-              + 'link VARCHAR(255),'
-              + 'item VARCHAR(255),'
-              + 'stock VARCHAR(255)'
-              +  ')', function (err) {
-                  if (err) throw err;
-              });
-      });
+      // connection.query('USE l80k3j1waol9ialw', function (err) {
+      //     if (err) throw err;
+      //     connection.query('CREATE TABLE IF NOT EXISTS storage('
+      //         + 'id INT NOT NULL AUTO_INCREMENT,'
+      //         + 'PRIMARY KEY(id),'
+      //         + 'link VARCHAR(255),'
+      //         + 'item VARCHAR(255),'
+      //         + 'stock VARCHAR(255)'
+      //         +  ')', function (err) {
+      //             if (err) throw err;
+      //         });
+      // });
 
 app.get('/', function (req, res) {
   res.render('index');
@@ -177,12 +177,35 @@ app.post('/scrape', function(req, res){
 
         
 
+      })
+        $('.server-error').filter(function() {
+              //Holds data
+        var data=$(this)
+
+        reviews = data.text();
+        createJson.reviews=reviews;
+        captureReview=reviews;
+
+        if(captureReview != "Reviews") {
+        console.log("Has"+ "" +captureReview)
+        var sql= "INSERT INTO storage (link, item, stock) VALUES ('" +createUrl.link +"','"+createItem.item+"','"+captureReview+"')"
+        connection.query(sql,
+        function (err, result) {
+            if (err) throw err;
+            console.log(req.body)
+            res.send('Link added to database with ID: ' + result.insertId + " " + "go to /index to see database");
+        }
+    );
+        } else {
+          console.log("no"+ "" +captureReview)
+        }
+
+        
+
      
 
       })
     } 
-
-
 
   })
     //        var sql= "INSERT INTO storage (link, item, stock) VALUES ('" +createUrl.link +"','"+createItem.item+"','"+createJson.reviews+"')"
@@ -223,6 +246,74 @@ app.delete('/delete', function(req,res){
     });
 });
 
+//update server data
+app.get('/update', function (req,res) {
+
+
+  URL=req.body.link
+  itemNumber=req.body.itemNum
+   console.log(itemNumber)
+    var createJson= {
+    reviews:"",
+  }
+  
+       request(URL, function(err, response, html) {
+
+      if(!err){
+       var $= cheerio.load(html);
+
+      var reviews;
+  
+      //captures target
+      $('#reviews-text').filter(function(){
+        //Holds data
+        var data=$(this)
+
+        reviews = data.text();
+        createJson.reviews=reviews;
+        captureReview=reviews;
+        console.log("I AM WORKING HERE" +" "+ reviews+ "with" + " "+ itemNumber)
+
+        if(captureReview === "Reviews") {
+        console.log("Has"+ "" +captureReview)
+        var sql= "UPDATE storage SET STOCK='" + createJson.reviews + "' "+"WHERE ID=" + itemNumber
+               connection.query(sql, function (err, result) {
+            if (err) throw err;
+   
+             res.redirect('/index');
+            // res.send('Link added to database with ID: ' + result.insertId + " " + "go to /index to see database");
+
+        })
+      }
+    });
+
+      $('.server-error').filter(function() {
+         var data=$(this)
+
+        reviews = data.text();
+        createJson.reviews=reviews;
+        captureReview=reviews;
+        console.log("I AM WORKING HERE" +" "+ reviews+ "with" + " "+ itemNumber)
+
+        if(captureReview === "Reviews") {
+        console.log("Has"+ "" +captureReview)
+        var sql= "UPDATE storage SET STOCK='" + createJson.reviews + "' "+"WHERE ID=" + itemNumber
+               connection.query(sql, function (err, result) {
+            if (err) throw err;
+   
+             res.redirect('/index');
+            // res.send('Link added to database with ID: ' + result.insertId + " " + "go to /index to see database");
+
+      })
+    }
+  })
+     }
+   }
+   )
+     })
+
+
+
 // Open Server
 var PORT = process.env.PORT || 3000;
 app.listen(process.env.PORT || 3000, function(){
@@ -230,58 +321,3 @@ app.listen(process.env.PORT || 3000, function(){
 });
 
 
-//update server data
-// app.get('/update', function req,res) {
-
-//   connection.query('UPDATE link FROM storage', function (err, result) {
-//     if (err) throw err;
-//     res.send(url)
-//   })
-
-//   for (var i =0;i< url.length; i ++) {
-//        request(url, function(err, response, html) {
-
-//       if(!err){
-//        var $= cheerio.load(html);
-
-//       var reviews;
-  
-//       //captures target
-//       $('#reviews-text').filter(function(){
-//         //Holds data
-//         var data=$(this)
-
-//         reviews = data.text();
-//         createJson.reviews=reviews;
-//         captureReview=reviews;
-
-//         if(captureReview === "Reviews") {
-//           console.log("Has"+ "" +captureReview)
-//         var sql= "INSERT INTO storage (link, item, stock) VALUES ('" +createUrl.link +"','"+createItem.item+"','"+captureReview+"')"
-//         connection.query(sql,
-//         function (err, result) {
-//             if (err) throw err;
-//             console.log(req.body)
-//             res.send('Link added to database with ID: ' + result.insertId + " " + "go to /index to see database");
-//         }
-//     );
-//         } else {
-//           console.log("no"+ "" +captureReview)
-//         }
-
-        
-
-     
-
-//       })
-//     } 
-
-
-
-//   })
-
-//   }
-
-
-// }
-//   
